e0db8ad2a13b1b1e9064ecf7b2ebd847e561fd5b
MatheusKristman <kristman058@gmail.com>
2022-12-31 19:03:43 -0300

    added previous button function

--- src/pages/components/ResultSection.jsx
+++ src/pages/components/ResultSection.jsx
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from 'react';
 
 function ResultSection({ tryAgain, setTryAgain }) {
   const [resultStarted, setResultStarted] = useState(false);
+  const [isPrevArrEmpty, setIsPrevArrEmpty] = useState(true);
   const [name, setName] = useState('');
   const [savedNames, setSavedNames] = useState([]);
+  const [savedNamesBackup, setSavedNamesBackup] = useState([]);
 
   useEffect(() => {
     function fetchNames() {
-      const arr = JSON.parse(localStorage.getItem('names'));
+      const arr = JSON.parse(localStorage.getItem('names')) || [];
 
       setSavedNames(arr);
     }
@@ -19,20 +21,61 @@ function ResultSection({ tryAgain, setTryAgain }) {
     return arr.length === 0
   }
 
+  function isPrevArrayAboveOneElement(arr) {
+    return arr.length <= 1;
+  }
+
   function getName() {
     setResultStarted(true);
     const nameArr = [...savedNames];
-
+    const nameBackup = [...savedNamesBackup];
+    
     if (isArrayEmpty(nameArr)) {
       setName('Clique em Embaralhar Novamente para reiniciar');
       setTryAgain(true);
       return;
     }
 
+    nameBackup.push(nameArr[0]);
+    setSavedNamesBackup(nameBackup);
     setName(nameArr.shift());
     setSavedNames(nameArr);
     localStorage.setItem('names', JSON.stringify(nameArr));
   }
+
+  function getPreviousName() {
+    const nameArr = [...savedNames];
+    const nameBackup = [...savedNamesBackup];
+    
+    if (isArrayEmpty(nameArr) && tryAgain) {
+      setTryAgain(false);
+      setName(nameBackup[nameBackup.length - 1]);
+      return;
+    }
+    
+    if (!nameArr.includes(nameBackup[nameBackup.length - 1])) {
+      console.log("ja tem no array principal")
+      setSavedNames([nameBackup.pop() , ...savedNames]);
+    }
+
+    setName(nameBackup[nameBackup.length - 1]);
+    setSavedNamesBackup(nameBackup);
+    localStorage.setItem('names', JSON.stringify(nameArr));
+  }
+
+  useEffect(() => {
+    function prevArrayAboveWithOneElement() {
+      if (isPrevArrayAboveOneElement(savedNamesBackup)) {
+        setIsPrevArrEmpty(true);
+        console.log('é menor')
+        return;
+      }
+      console.log('pode executar a função')
+      setIsPrevArrEmpty(false);
+    }
+
+    prevArrayAboveWithOneElement();
+  }, [savedNamesBackup])
 
   return (
     <section className='result wrapper'>
@@ -45,6 +88,7 @@ function ResultSection({ tryAgain, setTryAgain }) {
           )}
         </div>
         <button onClick={getName} className={tryAgain ? 'start-btn disabled' : 'start-btn'} disabled={tryAgain}>{resultStarted ? 'Proximo' : 'Começar'}</button>
+        <button onClick={getPreviousName} className={isPrevArrEmpty ? 'previous-btn disabled' : 'previous-btn'} disabled={isPrevArrEmpty}>Anterior</button>
       </div>
     </section>
   );
